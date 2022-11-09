e9a9d32c80b9832fc5ca4516d3dc1b6fc8768a35
Gonzalo Borderolle <gborderolle@gmail.com>
2022-11-09 15:26:50 -0300

    .

---
+++ Chempe/wwwroot/js/pages/new_request.js
@@ -0,0 +1,21 @@
+﻿/*
+DTO_New_request
+
+    model.List_DTO_List_warrants_type
+
+*/
+
+$(function () {
+});
+
+function ddl_warrant_type_onChange() {
+    var ddl_warrant_type = $("#ddl_warrant_type :selected").text();
+    if (ddl_warrant_type !== null && ddl_warrant_type !== undefined) {
+
+        var List_DTO_List_warrants_type = model.List_DTO_List_warrants_type;
+        if (List_DTO_List_warrants_type !== null && List_DTO_List_warrants_type !== undefined) {
+            
+        }
+    }
+
+}
